ec410bc9806835a1900511cf66019a78ba2e45fa
nadezda Demkina <demkina98@mail.ru>
2021-12-15 03:23:40 +0300

    modified build script

--- build.sh
+++ build.sh
@@ -4,12 +4,31 @@ rm -rf site
 rm -rf site.zip
 
 mkdir site
+
 cp -r common/ site/common
+rm site/common/.DS_Store
+rm site/common/images/.DS_Store
+
 cp -r conception/ site/conception
+rm site/conception/.DS_Store
+rm site/conception/images/.DS_Store
+
 cp -r elements/ site/elements
+rm site/elements/.DS_Store
+rm site/elements/images/.DS_Store
+
 cp -r flat/ site/flat
+rm site/flat/.DS_Store
+rm site/flat/images/.DS_Store
+
 cp -r spatio-temporal/ site/spatio-temporal
+rm temporal/ site/spatio-temporal/.DS_Store
+rm temporal/ site/spatio-temporal/images/.DS_Store
+
 cp -r volume/ site/volume
+rm site/volume/.DS_Store
+rm site/volume/images/.DS_Store
+
 cp composition.html site/composition.html
 cp construction.html site/construction.html
 cp content.html site/content.html
@@ -20,3 +39,5 @@ cp strategy.html site/strategy.html
 cp structure.html site/structure.html
 
 zip -r site.zip site
+
+rm -rf site/
